da8ad5c3ebe2d71f7cfe6fdcf68d3813e4c65b06
Mark Anthony <marcuma@fgmail.com>
2025-02-19 23:15:43 -0500

    Final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ClientAddFormComponent} from './client-add-form/client-add-form.component';
+import {MeetingAddFormComponent} from './meeting-add-form/meeting-add-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'addclient',
+    component: ClientAddFormComponent,
+    title: 'Client Add Form',
+  },
+  {
+    path: 'addmeeting',
+    component: MeetingAddFormComponent,
+    title: 'Meeting Add Form',
+  }
+];
